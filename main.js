d10037054bbf849354c5befbe9490c86dd34b21a
FoxxIgor <110695245+FoxxIgor@users.noreply.github.com>
2023-03-20 16:56:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function detectarTv(){
+    window.location = "tv.html";
+}
+function detectarMesa(){
+    console.log("mesa");
+    window.location = "mesa.html";
+}
+function detectarBanana(){
+    window.location = "banana.html";
+}
---
+++ tv.js
@@ -0,0 +1,48 @@
+var img = "";
+var objects = [];
+var statusAtual = false;
+
+function preload(){
+    img = loadImage("tvimg.jpg");
+}
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    
+}
+function modelLoaded(){
+    console.log("Modelo carregado");
+    statusAtual = true;
+    objectDetector.detect(img, gotResults)
+}
+function gotResults(error, result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        objects = result;
+    }
+}
+function voltar(){
+    window.location= "index.html";
+}
+
+function draw(){
+    image(img, 0, 0, 400, 400);
+    if(statusAtual){
+        document.querySelector("#status").innerHTML = "Status: Objeto Detectado";
+        for(var i=0; i<objects.length; i++){
+            porc = floor(objects[i].confidence*100);
+            width = floor(objects[i].width);
+            height = floor(objects[i].height);
+            x = floor(objects[i].x);
+            y = floor(objects[i].y)-150;
+            noFill();
+            text(objects[i].label+" "+porc+"%", x, y);
+            stroke("#FF0000");
+            rect(x, y, width, height);
+        }
+    }
+}
